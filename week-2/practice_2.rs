3d901aaaf39147712371a4d6243eeef55f71c3b6
tomiwa-pau <ireoluwatomiwa.atiba@pau.edu.ng>
2026-08-31 23:34:51 +0100

    My entire week 2 file

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+ fn main() {
+
+    println!(); // prints just a
+
+    println!("hello ");//prints
+
+    println!("format {} arguments", "some"); //prints format some arguments
+
+    println!("My name is {}. I am a student of COS 101", "Ireoluwatomiwa Adeeyo Atiba");       
+
+ }
